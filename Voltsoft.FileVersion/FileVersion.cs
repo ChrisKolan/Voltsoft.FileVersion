bcb1fe040aa1550d7452bd3e010abc832e1372b6
ChrisKolan <chris.kolan@gmail.com>
2019-05-11 13:56:17 +0200

    Add project files.

---
+++ Voltsoft.FileVersion/FileVersion.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Voltsoft.FileVersion
+{
+    public static class FileVersion
+    {
+        public static string Get(string pathToFile)
+        {
+            string fileVersion;
+
+            try
+            {
+                var versionInfo = FileVersionInfo.GetVersionInfo(pathToFile);
+                fileVersion = versionInfo.FileVersion;
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                fileVersion = null;
+            }
+            return fileVersion;
+        }
+    }
+}
